2d2d72c39fa93105cf66b1892131151fc6f586ad
Muhammad-Abid11 <abidkhan11845@gmail.com>
2024-05-14 10:36:27 +0500

    Options,Fonts Styling,

--- app/page.jsx
+++ app/page.jsx
@@ -27,7 +27,6 @@ export default function Home() {
     const difficulty = renderQuestion.difficulty;
     const stars = difficulty === "easy" ? 1 : difficulty === "hard" ? 5 : 3;
     setStars(stars);
-
   }, []);
 
   const next = () => {
@@ -40,11 +39,10 @@ export default function Home() {
     ];
     const shuffledOptions = shuffle(arr);
     setQuestionOptions(shuffledOptions);
-    
-    const difficulty = question[currentIndex+1].difficulty;
+
+    const difficulty = question[currentIndex + 1].difficulty;
     const stars = difficulty === "easy" ? 1 : difficulty === "hard" ? 5 : 3;
     setStars(stars);
-
   };
 
   const shuffle = (array) => {
@@ -79,8 +77,8 @@ export default function Home() {
     }
   };
 
-  console.log("scoree",score)
-
+  console.log("scoree", score);
+  const decodedQuestion = decodeURIComponent(renderQuestion.question);
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-success">
@@ -95,42 +93,39 @@ export default function Home() {
         </div>
 
         <div>
-          <h1>
+          <h1 className="text-3xl">
             Qestion {currentIndex + 1} of {question.length}
           </h1>
           <p>Entertainment: Board Games</p>
           <StarRating difficulty={stars} />
         </div>
-        <div className="flex justify-center w-30">
-          {renderQuestion.question.replace(/%20/g, " ")}
+        <div className="flex justify-center w-30 text-2xl max-w-xl">
+          {decodedQuestion}
         </div>
 
         <div className="grid grid-cols-2 gap-4">
-          <ul>
-            {questionOptions.map((data, i) => {
-              return (
-                <li key={i}>
-                  <button
-                    disabled={selectAns}
-                    style={
-                      selectAns === data
-                        ? { backgroundColor: "gray", color: "white" }
-                        : {}
-                    }
-                    onClick={(e) => ansCheck(e)}
-                    type="button"
-                    class="py-2.5 px-5 me-2 mb-2 text-sm font-medium text-gray-900 
-                focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 
-                hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 
-                dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white 
-                dark:hover:bg-gray-700"
-                  >
-                    {data}
-                  </button>
-                </li>
-              );
-            })}
-          </ul>
+          {questionOptions.map((data, i) => {
+            return (
+              <button
+                key={i}
+                disabled={selectAns}
+                style={
+                  selectAns === data
+                    ? { backgroundColor: "gray", color: "white" }
+                    : {}
+                }
+                onClick={(e) => ansCheck(e)}
+                type="button"
+                className="py-2.5 px-5 mb-2 text-sm font-medium text-gray-900 
+        focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 
+        hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 
+        dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white 
+        dark:hover:bg-gray-700"
+              >
+                {data.replace(/%20/g, " ")}
+              </button>
+            );
+          })}
         </div>
 
         <div>
@@ -140,28 +135,52 @@ export default function Home() {
             <button onClick={restart}>Restart</button>
           ) : (
             selectAns && (
-              <div>
-                {isCorrect ? <h1>Correct</h1> : <h1>InCorrect</h1>}
-                <button onClick={next}>Next</button>
-              </div>
+              <div className="flex flex-col items-center">
+  <div className="flex justify-center items-center">
+    {isCorrect ? (
+      <h1 className="lg:text-3xl">Correct</h1>
+    ) : (
+      <h1 className="lg:text-3xl">Incorrect</h1>
+    )}
+  </div>
+
+  <div>
+    <button
+      onClick={next}
+      className="mt-4 py-2 px-4 bg-gray-200 text-black font-semibold rounded-lg shadow-md hover:bg-gray-500 hover:text-white focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-75"
+    >
+      Next
+    </button>
+  </div>
+</div>
+
             )
           )}
         </div>
 
-        
         <div className="flex">
-      <div className="flex-grow bg-green-500" role="progressbar" style={{ width: `${(score/currentIndex) * 10}%` }}>
-       Score {(score/(currentIndex+1).toFixed())*10}
-      </div>
-      <div className="flex-grow bg-yellow-500" role="progressbar" style={{ width: `${score/questions.length * 10}%` }}>
-        Maximum {(score/questions.length)*10}
-      </div>
-      <div className="flex-grow bg-red-500" role="progressbar" style={{ width: `${currentIndex/questions.length * 10}%` }}>
-         {(currentIndex/questions.length)*10}
-      </div>
-    </div>
-
-        
+          <div
+            className="flex-grow bg-green-500"
+            role="progressbar"
+            style={{ width: `${(score / currentIndex) * 10}%` }}
+          >
+            Score {(score / (currentIndex + 1).toFixed()) * 10}
+          </div>
+          <div
+            className="flex-grow bg-yellow-500"
+            role="progressbar"
+            style={{ width: `${(score / questions.length) * 10}%` }}
+          >
+            Maximum {(score / questions.length) * 10}
+          </div>
+          <div
+            className="flex-grow bg-red-500"
+            role="progressbar"
+            style={{ width: `${(currentIndex / questions.length) * 10}%` }}
+          >
+            {(currentIndex / questions.length) * 10}
+          </div>
+        </div>
       </div>
     </main>
   );
